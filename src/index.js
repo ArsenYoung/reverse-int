4e398068222854229f159b687289e1f13dfbe840
arsenyoung <molodoiars@gmail.com>
2023-10-23 20:07:49 +0400

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    if (n < 0) {
+        n *= -1;
+    }
+    while (n > 0) {
+      result += Math.floor(n % 10);
+      n = Math.floor(n / 10);
+    }
+    return result;
 }
